b833beae8f8b3d493d1e00faef05744af4bf07cd
sun5700x <1364738426@qq.com>
2023-09-26 22:17:22 +0800

    导航栏头像框

--- src/pages/Home/navigation.tsx
+++ src/pages/Home/navigation.tsx
@@ -4,6 +4,9 @@ export default function Navigation(): JSX.Element {
   return (
     <>
       <nav className={home.nav}>
+        <div className={home.img}>
+          <img src="/src/assets/头像.jpg" />
+        </div>
         <ul>
           <li>首页</li>
           <li>日志</li>
