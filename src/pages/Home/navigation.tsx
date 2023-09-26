c3234dbf4e707c61771ce5feb91ae8d77e7931d5
sunshineLi2019 <1364738426@qq.com>
2023-09-26 19:10:37 +0800

    0926

--- src/pages/Home/navigation.tsx
+++ src/pages/Home/navigation.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import home from "./home.module.less";
 export default function Navigation(): JSX.Element {
   return (
-    <div className={home.nav}>
-      <nav>1</nav>
-    </div>
+    <>
+      <nav className={home.nav}>
+        <ul>
+          <li>首页</li>
+          <li>日志</li>
+          <li>项目</li>
+          <li>游戏</li>
+          <li>留言</li>
+        </ul>
+      </nav>
+    </>
   );
 }
