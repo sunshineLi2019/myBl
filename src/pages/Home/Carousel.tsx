2e9b10a97e489a830c510b8010d4166a7e6c8783
sun5700x <1364738426@qq.com>
2024-02-28 23:47:33 +0800

    230228

--- src/pages/Home/Carousel.tsx
+++ src/pages/Home/Carousel.tsx
@@ -4,7 +4,36 @@ export default function Carousel() {
   return (
     <div style={{ overflow: "hidden" }}>
       <ul className={article.box}>
-        <li>2024</li>
+        <li>
+          <div className={article.care}>
+            <div className={article.target}>
+              随笔
+            </div>
+            <div className={article.header}>
+              <span
+                className={`
+                  ${article.iconfont}
+                  ${article.iconTime}
+                  `}
+              ></span>
+              <span> 2024-01-05 13:30:42</span>
+              <span
+                className={article.see}
+              ></span>
+            </div>
+            <div className={article.content}>
+              <div className={article.title}>
+                <h3>2024展望</h3>
+              </div>
+              <div
+                className={article.simpleContent}
+              >
+                近况
+                好久没更新博客了，一方面是来到了长沙之后，人确实变得懒...
+              </div>
+            </div>
+          </div>
+        </li>
         <li>2025</li>
       </ul>
     </div>
