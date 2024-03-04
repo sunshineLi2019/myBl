f512916a25bafb5cbfe68c88c5f3072227eb989b
sun5700x <1364738426@qq.com>
2024-03-04 22:15:32 +0800

    增加分页+淡入动画

--- src/pages/Home/Carousel.tsx
+++ src/pages/Home/Carousel.tsx
@@ -10,20 +10,46 @@ export default function Carousel() {
               随笔
             </div>
             <div className={article.header}>
-              <span
-                className={`
+              <span className={article.time}>
+                <span
+                  className={`
                   ${article.iconfont}
                   ${article.iconTime}
                   `}
-              ></span>
-              <span> 2024-01-05 13:30:42</span>
-              <span
-                className={article.see}
-              ></span>
+                ></span>
+                <span>2024-01-05 13:30:42</span>
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                  ${article.iconfont}
+                  ${article.iconYanjing}
+                  `}
+                ></span>
+                106
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                ${article.iconfont}
+                ${article.iconGood}
+                `}
+                ></span>
+                123
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                ${article.iconfont}
+                ${article.iconShoucang}
+                `}
+                ></span>
+                123
+              </span>
             </div>
             <div className={article.content}>
               <div className={article.title}>
-                <h3>2024展望</h3>
+                2024展望
               </div>
               <div
                 className={article.simpleContent}
@@ -34,7 +60,311 @@ export default function Carousel() {
             </div>
           </div>
         </li>
-        <li>2025</li>
+        <li>
+          <div className={article.care}>
+            <div className={article.target}>
+              随笔
+            </div>
+            <div className={article.header}>
+              <span className={article.time}>
+                <span
+                  className={`
+                  ${article.iconfont}
+                  ${article.iconTime}
+                  `}
+                ></span>
+                <span>2024-01-05 13:30:42</span>
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                  ${article.iconfont}
+                  ${article.iconYanjing}
+                  `}
+                ></span>
+                106
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                ${article.iconfont}
+                ${article.iconGood}
+                `}
+                ></span>
+                123
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                ${article.iconfont}
+                ${article.iconShoucang}
+                `}
+                ></span>
+                123
+              </span>
+            </div>
+            <div className={article.content}>
+              <div className={article.title}>
+                2024展望
+              </div>
+              <div
+                className={article.simpleContent}
+              >
+                近况
+                好久没更新博客了，一方面是来到了长沙之后，人确实变得懒...
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className={article.care}>
+            <div className={article.target}>
+              随笔
+            </div>
+            <div className={article.header}>
+              <span className={article.time}>
+                <span
+                  className={`
+                  ${article.iconfont}
+                  ${article.iconTime}
+                  `}
+                ></span>
+                <span>2024-01-05 13:30:42</span>
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                  ${article.iconfont}
+                  ${article.iconYanjing}
+                  `}
+                ></span>
+                106
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                ${article.iconfont}
+                ${article.iconGood}
+                `}
+                ></span>
+                123
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                ${article.iconfont}
+                ${article.iconShoucang}
+                `}
+                ></span>
+                123
+              </span>
+            </div>
+            <div className={article.content}>
+              <div className={article.title}>
+                2024展望
+              </div>
+              <div
+                className={article.simpleContent}
+              >
+                近况
+                好久没更新博客了，一方面是来到了长沙之后，人确实变得懒...
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className={article.care}>
+            <div className={article.target}>
+              随笔
+            </div>
+            <div className={article.header}>
+              <span className={article.time}>
+                <span
+                  className={`
+                  ${article.iconfont}
+                  ${article.iconTime}
+                  `}
+                ></span>
+                <span>2024-01-05 13:30:42</span>
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                  ${article.iconfont}
+                  ${article.iconYanjing}
+                  `}
+                ></span>
+                106
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                ${article.iconfont}
+                ${article.iconGood}
+                `}
+                ></span>
+                123
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                ${article.iconfont}
+                ${article.iconShoucang}
+                `}
+                ></span>
+                123
+              </span>
+            </div>
+            <div className={article.content}>
+              <div className={article.title}>
+                2024展望
+              </div>
+              <div
+                className={article.simpleContent}
+              >
+                近况
+                好久没更新博客了，一方面是来到了长沙之后，人确实变得懒...
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className={article.care}>
+            <div className={article.target}>
+              随笔
+            </div>
+            <div className={article.header}>
+              <span className={article.time}>
+                <span
+                  className={`
+                  ${article.iconfont}
+                  ${article.iconTime}
+                  `}
+                ></span>
+                <span>2024-01-05 13:30:42</span>
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                  ${article.iconfont}
+                  ${article.iconYanjing}
+                  `}
+                ></span>
+                106
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                ${article.iconfont}
+                ${article.iconGood}
+                `}
+                ></span>
+                123
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                ${article.iconfont}
+                ${article.iconShoucang}
+                `}
+                ></span>
+                123
+              </span>
+            </div>
+            <div className={article.content}>
+              <div className={article.title}>
+                2024展望
+              </div>
+              <div
+                className={article.simpleContent}
+              >
+                近况
+                好久没更新博客了，一方面是来到了长沙之后，人确实变得懒...
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className={article.care}>
+            <div className={article.target}>
+              随笔
+            </div>
+            <div className={article.header}>
+              <span className={article.time}>
+                <span
+                  className={`
+                  ${article.iconfont}
+                  ${article.iconTime}
+                  `}
+                ></span>
+                <span>2024-01-05 13:30:42</span>
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                  ${article.iconfont}
+                  ${article.iconYanjing}
+                  `}
+                ></span>
+                106
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                ${article.iconfont}
+                ${article.iconGood}
+                `}
+                ></span>
+                123
+              </span>
+              <span className={article.see}>
+                <span
+                  className={`
+                ${article.iconfont}
+                ${article.iconShoucang}
+                `}
+                ></span>
+                123
+              </span>
+            </div>
+            <div className={article.content}>
+              <div className={article.title}>
+                2024展望
+              </div>
+              <div
+                className={article.simpleContent}
+              >
+                近况
+                好久没更新博客了，一方面是来到了长沙之后，人确实变得懒...
+              </div>
+            </div>
+          </div>
+        </li>
+        <div className={article.pagenation}>
+          <div className={article.page}>
+            <select name="" id="">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+          </div>
+          <div>
+            <span>&lt;</span>
+          </div>
+          <div className={article.black}>
+            <button>1</button>
+          </div>
+          <div>
+            <button>2</button>
+          </div>
+          <div>
+            <button>3</button>
+          </div>
+          <div>
+            <span>&gt;</span>
+          </div>
+        </div>
       </ul>
     </div>
   );
