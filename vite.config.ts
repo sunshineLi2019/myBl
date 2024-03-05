baf0ac2ce7b1c0752353319b9ec944d9462abd2c
sun5700x <1364738426@qq.com>
2024-03-05 21:45:41 +0800

    pages配置

--- vite.config.ts
+++ vite.config.ts
@@ -24,8 +24,10 @@ export default defineConfig({
       // 是对css模块化的默认行为进行覆盖
       localsConvention: "camelCase", // 修改生成的配置对象的key的展示形式(驼峰还是中划线形式)
       scopeBehaviour: "local", // 配置当前的模块化行为是模块化还是全局化 (有hash就是开启了模块化的一个标志, 因为他可以保证产生不同的hash值来控制我们的样式类名不被覆盖)
-      generateScopedName: "[name]_[local]_[hash:5]",
+      generateScopedName:
+        "[name]_[local]_[hash:5]",
       // globalModulePaths: ["./componentB.module.css"], // 代表不想参与到css模块化的路径
     },
   },
+  base: "/myBlog/",
 });
